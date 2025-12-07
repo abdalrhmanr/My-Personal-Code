b92d0f0f5e884126ea91f112611ec705cf1ba9f8
abdalrhmanr <cs.abdalrhman10@gmail.com>
2025-12-07 17:51:46 +0200

    Create Single_Linked_List [ Delete Last Node ].cpp

---
+++ Single_Linked_List [ Delete Last Node ].cpp
@@ -0,0 +1,65 @@
+
+using namespace std;
+
+struct Node{
+  int data;
+  Node* next;
+};
+
+
+void DeleteLastNode(Node * &head)
+{
+
+  if(head == NULL)
+  {
+    return;
+  }
+  
+  if(head->next == NULL)
+  {
+      delete head;
+      head = NULL; 
+      return;
+  }
+  
+  Node* Current = head, * Prev = head;
+  while(Current->next != NULL)
+  {
+    Prev = Current;
+    Current =  Current->next;
+  }
+ 
+  Prev->next = NULL;
+  delete Current;
+  return;
+}
+
+
+int main() 
+{
+  Node* head = NULL;
+  
+  Node* Node1 = new Node();
+  Node* Node2 = new Node();
+  Node* Node3 = new Node();
+ 
+ //-----------------------
+ 
+ Node1->data = 10;
+ Node2->data = 20;
+ Node3->data = 30;
+ 
+ //------------------------
+ 
+ Node1->next = Node2;
+ Node2->next = Node3;
+ Node3->next = NULL;
+ 
+ head = Node1;
+ //----------------
+ 
+DeleteLastNode(head);
+ 
+
+    return 0;
+}
