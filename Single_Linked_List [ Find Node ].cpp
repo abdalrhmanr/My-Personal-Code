bbce9b3f378f4f28a04dbb83ecbcb003540f9317
abdalrhmanr <cs.abdalrhman10@gmail.com>
2025-12-07 10:49:09 +0200

    Create Single_Linked_List [ Find Node ].cpp

---
+++ Single_Linked_List [ Find Node ].cpp
@@ -0,0 +1,59 @@
+using namespace std;
+
+struct Node{
+  int data;
+  Node* next;
+};
+
+bool FindNode(int value, Node * &head)
+{
+  Node* current = head;
+  
+  if(head == NULL)
+  {
+    return true;
+  }
+  
+  while(current != NULL)
+  {
+    if(current->data == value)
+    {
+      return 1;
+    }
+    
+    current = current->next;
+  }
+  
+  return false;
+}
+
+
+int main() 
+{
+  Node* head = NULL;
+  
+  Node* Node1 = new Node();
+  Node* Node2 = new Node();
+  Node* Node3 = new Node();
+ 
+ //-----------------------
+ 
+ Node1->data = 10;
+ Node2->data = 20;
+ Node3->data = 30;
+ 
+ //------------------------
+ 
+ Node1->next = Node2;
+ Node2->next = Node3;
+ Node3->next = NULL;
+ 
+ head = Node1;
+ //----------------
+ 
+ bool IsNodeFound = FindNode(10,head);
+ 
+cout << IsNodeFound << "\nHello, World!";
+  
+    return 0;
+}
