f56703d3e261a7d93a09124e33359bcb2d5c5c48
abdalrhmanr <cs.abdalrhman10@gmail.com>
2025-12-07 17:38:04 +0200

    Create Single_Linked_List [ Delete First Node ].cpp

---
+++ Single_Linked_List [ Delete First Node ].cpp
@@ -0,0 +1,49 @@
+struct Node{
+  int data;
+  Node* next;
+};
+
+void DeleteFirstNode(Node * &head)
+{
+  
+   Node* Current = head;
+  
+  if(head == NULL)
+  {
+    return;
+  }
+  
+ 
+   head =  Current->next;
+   delete Current;
+   return;
+}
+
+int main()
+{
+
+  Node* head = NULL;
+  
+  Node* Node1 = new Node();
+  Node* Node2 = new Node();
+  Node* Node3 = new Node();
+ 
+ //-----------------------
+ 
+ Node1->data = 10;
+ Node2->data = 20;
+ Node3->data = 30;
+ 
+ //------------------------
+ 
+ Node1->next = Node2;
+ Node2->next = Node3;
+ Node3->next = NULL;
+ 
+ head = Node1;
+ //----------------
+ 
+DeleteFirstNode(20,head);
+
+
+}
